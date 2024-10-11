d0eeb3fe4661f872a9e807ff4243337040b69dc2
gagandeepsingh97 <gagandeepsingh2911@gmail.com>
2024-10-11 14:12:26 -0400

    Initial commit

---
+++ src/main/java/com/bookcook/bookcook/BookcookApplication.java
@@ -0,0 +1,13 @@
+package com.bookcook.bookcook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookcookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookcookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookcook/bookcook/BookcookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookcook.bookcook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookcookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
